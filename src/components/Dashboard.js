a28a77ca0269b43e6febe362789ff4a78a5d809b
Isaac <duyilekan98@gmail.com>
2022-09-29 22:39:44 +0100

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,6 +29,7 @@ import eye from './Images/eye.png';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
+import {Link} from 'react-router-dom';
 
 
 function Dashboard() {
@@ -53,9 +54,9 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className='navigation-link'>
-                                <div className='navigation-link-inner'>
+                                <Link to='/Schedule' className='navigation-link-inner'>
                                     <img src={schedule} alt="User" className='nav-image'></img><span>Schedule</span>
-                                </div>
+                                </Link>
                             </div>
                             <div className='navigation-link'>
                                 <div className='navigation-link-inner'>
