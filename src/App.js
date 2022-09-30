17775d8bef6b2dc20cc8caec048201fe866ddc59
Isaac <duyilekan98@gmail.com>
2022-09-30 07:24:45 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
+import Schedule from "./components/Schedule";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <React.Fragment>
-      <Dashboard/>
-    </React.Fragment>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}></Route>
+        <Route path='/Schedule' element={<Schedule/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
