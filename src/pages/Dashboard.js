7684187a5a4958c975eaffbe17b88ec2b138552a
Isaac <duyilekan98@gmail.com>
2022-10-11 10:00:19 +0100

    Add files via upload

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '../components/myStyles.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import 'bootstraps/dist/css/bootstrap.css';
@@ -12,7 +12,6 @@ import arrowdownblue from '../components/Images/arrow-down-blue.png';
 import arrowright from '../components/Images/arrow-right.png';
 import buttonarrowright from '../components/Images/button-arrow.png';
 import bluebell from '../components/Images/blue-bell.png';
-import graph from '../components/Images/graph.png';
 import male from '../components/Images/male.png';
 import female from '../components/Images/female.png';
 import trans from '../components/Images/trans.png';
@@ -21,9 +20,83 @@ import eye from '../components/Images/eye.png';
 import Navigation from '../components/Navigation';
 import Calendars from '../components/Calendars';
 import Header from '../components/Header';
+import lossarrow from '../components/Images/loss-arrow.png';
+import gainarrow from '../components/Images/gain-arrow.png';
+import { PieChart } from "react-minimal-pie-chart";
+import { Bar } from "react-chartjs-2";
+import Chart from 'chart.js/auto';
 
 
 function Dashboard() {
+    
+    const profitChartRef = useRef();
+    if (profitChartRef?.current) {
+        profitChartRef.current.chartInstance.destroy();
+    }
+    let data = [
+        { title: "One", value: 1, color: "#FF8080" },
+        { title: "Two", value: 2, color: "#00CFDE" },
+        { title: "Three", value: 3, color: "#5A5EED" },
+        { title: "Four", value: 4, color: "#20CB85" },
+    ];
+    const groupeddata = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
+          { 
+            borderColor: "rgba(0,0,0,0)",
+            borderWidth: 1.5,
+            barThickness: 6,
+            maxBarThickness: 6,
+            minBarLength: 2,
+            label: 'First dataset',
+            backgroundColor: '#06A77D',
+            data: [25, 15, 50, 41, 56, 55, 40, 35, 40, 55, 28, 32]
+          },
+          {
+            borderColor: "rgba(0,0,0,0)",
+            borderWidth: 1.5,
+            barThickness: 6,
+            maxBarThickness: 6,
+            minBarLength: 2,
+            label: 'Second dataset',
+            backgroundColor: '#FF8080',
+            data: [10, 5, 10, 11, 16, 45, 20, 55, 59, 20, 31, 56]
+          }
+        ],
+        borderWidth: 1
+    }
+    const options = {
+        responsive: true,
+        barValueSpacing: 20,
+        plugins: {
+            legend: {
+              display: false,
+            },
+        },
+        type:'bar',
+        scales: {
+            x: {
+              grid: {
+                display: false,
+              },
+            },
+            y: {
+              grid: {
+                display: false
+              },
+            },
+        },
+        layout: {
+            padding: {
+                left: 0
+            }
+        },
+        tooltips: {
+            callbacks: {
+               title: function() {}
+            }
+         },
+    }
     return(
         <div className='body'>
             <Container fluid>
@@ -63,7 +136,9 @@ function Dashboard() {
                                             <div className='one'>
                                                 <Row>
                                                     <Col lg={5} md={5} xs={6}>
-                                                        <img src={statsperson} alt="Person" className='schedule-image'></img>
+                                                        <div className='schedule-image-div schedule-image-div-one'>
+                                                            <img src={statsperson} alt="Person" className='schedule-image'></img>
+                                                        </div>
                                                     </Col>
                                                     <Col lg={7} md={7} xs={6}>
                                                         <p className='all-time'>All time <img src={arrowdown} alt="Arrow" className='arrow-down'></img></p>
@@ -71,14 +146,16 @@ function Dashboard() {
                                                 </Row>
                                                 <p className='number'>12,345</p>
                                                 <p className='patients'>Total Patients</p>
-                                                <p className='last-month'>5% less than last month</p>
+                                                <p className='last-month'><img src={lossarrow} className='stats-arrow' alt='Loss Arrow'></img><span>5% less than last month</span></p>
                                             </div>
                                         </Col>
                                         <Col md={4} lg={4} xs={12} className="stats-col-two">
                                             <div className='one'>
                                                 <Row>
                                                     <Col lg={6} md={6} xs={6}>
-                                                        <img src={calendarimage} alt="Person" className='appointment-image'></img>
+                                                        <div className='schedule-image-div schedule-image-div-two'>
+                                                            <img src={calendarimage} alt="Person" className='appointment-image'></img>
+                                                        </div>
                                                     </Col>
                                                     <Col lg={6} md={6} xs={6}>
                                                         <p className='all-time'>Today <img src={arrowdown} alt="Arrow" className='arrow-down'></img></p>
@@ -86,14 +163,16 @@ function Dashboard() {
                                                 </Row>
                                                 <p className='number'>25</p>
                                                 <p className='patients'>Appointments</p>
-                                                <p className='last-month'>15% more than yesterday</p>
+                                                <p className='last-month'><img src={gainarrow} className='stats-arrow' alt='Gain Arrow'></img><span>15% more than yesterday</span></p>
                                             </div>
                                         </Col>
                                         <Col md={4} lg={4} xs={12} className="stats-col">
                                             <div className='one'>
                                                 <Row>
                                                     <Col lg={6} md={6} xs={6}>
-                                                        <img src={clock} alt="Person" className='waiting-image'></img>
+                                                        <div className='schedule-image-div schedule-image-div-three'>
+                                                            <img src={clock} alt="Person" className='waiting-image'></img>
+                                                        </div>
                                                     </Col>
                                                     <Col lg={6} md={6} xs={6}>
                                                         <p className='all-time'>Today <img src={arrowdown} alt="Arrow" className='arrow-down'></img></p>
@@ -101,7 +180,7 @@ function Dashboard() {
                                                 </Row>
                                                 <p className='number'>21,045</p>
                                                 <p className='patients'>Waiting Room</p>
-                                                <p className='last-month'>35% less than yesterday</p>
+                                                <p className='last-month'><img src={gainarrow} className='stats-arrow' alt='Gain Arrow'></img><span>35% less than yesterday</span></p>
                                             </div>
                                         </Col>
                                     </Row>
@@ -170,8 +249,13 @@ function Dashboard() {
                                             <p className='patients-this'>Show <span className='patients-month'>by month</span> <img src={arrowdownblue} alt="Arrow" className='arrow-down'></img></p>
                                         </Col>
                                     </Row>
-                                    <div>
-                                        <img src={graph} alt='Graph' className='graph'></img>
+                                    <div className='bar-chart'>
+                                        <Bar
+                                            data={groupeddata}
+                                            width={null}
+                                            height={null}
+                                            options={options}
+                                        />
                                     </div>
                                 </div>
                             </Col>
@@ -192,9 +276,22 @@ function Dashboard() {
                                             </Row>
                                             <Row>
                                                 <Col lg={6} md={5} xs={5} className='patients-col patients-col-expand'>
-                                                    <div className='patients-div'>
-                                                        <p className='patients-number'>212,345</p>
-                                                        <p className='patients-para'>Patients</p>
+                                                <div className="">
+                                                        <PieChart
+                                                            center={[50, 50]}
+                                                            data={data}
+                                                            lineWidth={8}
+                                                            lengthAngle={360}
+                                                            paddingAngle={0}
+                                                            radius={50}
+                                                            startAngle={0}
+                                                            viewBoxSize={[100, 100]}
+                                                        >
+                                                            <g id="UrTavla">
+                                                                <text x="50%" y="45%" textAnchor="middle" stroke="#1D2B4F" strokeWidth="0.4px" dy=".3em" className='pie-number'>200,000</text>
+                                                                <text x="50%" y="60%" textAnchor="middle" stroke="#8692A6" strokeWidth="0.3px" dy=".8em" className='pie-patient'>Patients</text>
+                                                            </g>
+                                                        </PieChart>
                                                     </div>
                                                 </Col>
                                                 <Col lg={6} md={6} xs={6} className='patients-col'>
@@ -219,26 +316,26 @@ function Dashboard() {
                                                 </Col>
                                             </Row>
                                             <Row className='notification-each'>
-                                                <Col md={2} lg={2} xs={2}>
+                                                <Col md={2} lg={2} xs={2} className='notification-image-col'>
                                                     <img src={calendarimage} alt="Person" className='activities-icon'></img>
                                                 </Col>
-                                                <Col md={10} lg={10} xs={10}>
+                                                <Col md={10} lg={10} xs={10} className='notification-text-col'>
                                                     <span className='notification-span'>There are 3 new ready reports for <span className='notification-name'>Tosin Odubela</span></span>
                                                 </Col>
                                             </Row>
                                             <Row className='notification-each'>
-                                                <Col md={2} lg={2} xs={2}>
+                                                <Col md={2} lg={2} xs={2} className='notification-image-col'>
                                                     <img src={calendarimage} alt="Person" className='activities-icon'></img>
                                                 </Col>
-                                                <Col md={10} lg={10} xs={10}>
+                                                <Col md={10} lg={10} xs={10} className='notification-text-col'>
                                                     <span className='notification-span'>There are 3 new ready reports for <span className='notification-name'>Tosin Odubela</span></span>
                                                 </Col>
                                             </Row>
                                             <Row className='notification-each'>
-                                                <Col md={2} lg={2} xs={2}>
+                                                <Col md={2} lg={2} xs={2} className='notification-image-col'>
                                                     <img src={calendarimage} alt="Person" className='activities-icon'></img>
                                                 </Col>
-                                                <Col md={10} lg={10} xs={10}>
+                                                <Col md={10} lg={10} xs={10} className='notification-text-col'>
                                                     <span className='notification-span'>There are 3 new ready reports for <span className='notification-name'>Tosin Odubela</span></span>
                                                 </Col>
                                             </Row>
